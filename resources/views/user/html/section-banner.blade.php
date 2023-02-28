7fcac52bb89c36d17f5c1ebb15fe25e99b28242b
Administrator <tranvanthanh060699@gmail.com>
2023-02-28 19:18:33 +0700

    UP LAN 6

---
+++ resources/views/user/html/section-banner.blade.php
@@ -0,0 +1,37 @@
+<section class="banner-area-2 pt-200 pb-95" id="banner_animation"
+         style="background-size: auto; background-position: top left;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="banner-content">
+                    <img data-parallax='{"x": 0, "y": 250, "rotateZ":0}' class="shape"
+                         src="/user/html/img/banner/shape-3.png" alt="shape">
+                    <h1 class="wow fadeInUp mb-0">Các khoản vay và gửi tiết kiệm với rất nhiều ưu đãi khủng</h1>
+                    <p class="wow fadeInUp mt-50" data-wow-delay="0.3s">Modern Bank là ngân hàng mà bạn có thể tin tưởng
+                    sẽ hỗ trợ bạn mọi lúc bằng cách cung cấp các tiện ích cho vay mua nhà...</p>
+                    <a href="loan.html" data-wow-delay="0.5s"
+                       class="wow fadeInUp theme-btn theme-btn-lg mt-50">Áp dụng ngay bây giờ<i class="arrow_right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class=" col-md-6 col-lg-5 offset-lg-1 pt-40">
+                <div class="banner-img">
+                    <img class="main-img img-fluid wow fadeInRight" src="/user/html/img/banner/banner-img-2.png"
+                         alt="banner-img">
+                    <div class="shapes">
+                        <img data-parallax='{"x": 0, "y": 130, "rotateZ":0}' class="shape-1"
+                             src="/user/html/img/banner/shape-1.png" alt="shape">
+                        <img data-parallax='{"x": 0, "y": -130, "rotateZ":0}' class="shape-2"
+                             src="/user/html/img/banner/shape-2.png" alt="shape">
+                        <img data-parallax='{"x": 250, "y":0, "rotateZ":0}' class="shape-3"
+                             src="/user/html/img/banner/shape-4.png" alt="shape">
+                        <img data-parallax='{"x": -200, "y": 250, "rotateZ":0}' class="shape-4"
+                             src="/user/html/img/banner/shape-5.png" alt="shape">
+                        <img class="shape-5" src="/user/html/img/banner/shape-6.png" alt="shape">
+                        <img class="shape-6" src="/user/html/img/banner/shape-7.png" alt="shape">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
